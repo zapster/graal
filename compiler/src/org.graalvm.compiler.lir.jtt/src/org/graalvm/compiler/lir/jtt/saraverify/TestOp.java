43bfa5e9a6f8564cab7ab7e58058a95130d28782
gabriel <gabriel@gabriel-XPS-M1530>
2017-11-08 18:13:31 +0100

    Add TestMoveFromReg instruction

--- compiler/src/org.graalvm.compiler.lir.jtt/src/org/graalvm/compiler/lir/jtt/saraverify/TestOp.java
+++ compiler/src/org.graalvm.compiler.lir.jtt/src/org/graalvm/compiler/lir/jtt/saraverify/TestOp.java
@@ -1,23 +1,26 @@
 package org.graalvm.compiler.lir.jtt.saraverify;
 
+import static org.graalvm.compiler.lir.LIRInstruction.OperandFlag.HINT;
 import static org.graalvm.compiler.lir.LIRInstruction.OperandFlag.ILLEGAL;
 import static org.graalvm.compiler.lir.LIRInstruction.OperandFlag.REG;
+import static org.graalvm.compiler.lir.LIRInstruction.OperandFlag.STACK;
 
 import org.graalvm.compiler.lir.LIRInstruction;
 import org.graalvm.compiler.lir.LIRInstructionClass;
 import org.graalvm.compiler.lir.Opcode;
+import org.graalvm.compiler.lir.StandardOp.ValueMoveOp;
 import org.graalvm.compiler.lir.asm.CompilationResultBuilder;
 
 import jdk.vm.ci.meta.AllocatableValue;
 import jdk.vm.ci.meta.Value;
 
 public class TestOp {
-    public static class TestDefOp extends LIRInstruction {
+    public static class TestDef extends LIRInstruction {
 
-        public static final LIRInstructionClass<TestDefOp> TYPE = LIRInstructionClass.create(TestDefOp.class);
+        public static final LIRInstructionClass<TestDef> TYPE = LIRInstructionClass.create(TestDef.class);
         @Def protected AllocatableValue value;
 
-        public TestDefOp(AllocatableValue value) {
+        public TestDef(AllocatableValue value) {
             super(TYPE);
             this.value = value;
         }
@@ -28,6 +31,33 @@ public class TestOp {
 
         @Override
         public void emitCode(CompilationResultBuilder crb) {
+        }
+    }
+
+    public static class TestMoveFromReg extends LIRInstruction implements ValueMoveOp {
+        public static final LIRInstructionClass<TestMoveFromReg> TYPE = LIRInstructionClass.create(TestMoveFromReg.class);
+
+        @Def({REG, STACK}) protected AllocatableValue result;
+        @Use({REG, HINT}) protected AllocatableValue input;
+
+        public TestMoveFromReg(AllocatableValue result, AllocatableValue input) {
+            super(TYPE);
+            this.result = result;
+            this.input = input;
+        }
+
+        @Override
+        public void emitCode(CompilationResultBuilder crb) {
+        }
+
+        @Override
+        public AllocatableValue getResult() {
+            return result;
+        }
+
+        @Override
+        public AllocatableValue getInput() {
+            return input;
         }
     }
 
