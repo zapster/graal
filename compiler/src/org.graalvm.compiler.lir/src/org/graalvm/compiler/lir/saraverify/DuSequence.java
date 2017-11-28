6380ff54964f772f90f9be37165c59550dd73ea1
gabriel <gabriel@gabriel-XPS-M1530>
2017-11-28 19:11:53 +0100

    override toString() method

--- compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/saraverify/DuSequence.java
+++ compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/saraverify/DuSequence.java
@@ -1,52 +1,86 @@
 package org.graalvm.compiler.lir.saraverify;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
+import jdk.vm.ci.meta.AllocatableValue;
+
 public class DuSequence {
 
-    private LinkedList<DuPair> duPairs;
+	private LinkedList<DuPair> duPairs;
 
-    public DuSequence(DuPair duPair) {
-        duPairs = new LinkedList<>();
-        duPairs.add(duPair);
-    }
+	public DuSequence(DuPair duPair) {
+		duPairs = new LinkedList<>();
+		duPairs.add(duPair);
+	}
 
-    public void addFirst(DuPair duPair) {
-        duPairs.addFirst(duPair);
-    }
+	public void addFirst(DuPair duPair) {
+		duPairs.addFirst(duPair);
+	}
 
-    public DuPair peekFirst() {
-        return duPairs.peekFirst();
-    }
+	public DuPair peekFirst() {
+		return duPairs.peekFirst();
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof DuSequence)) {
-            return false;
-        }
+	public DuPair peekLast() {
+		return duPairs.peekLast();
+	}
 
-        DuSequence duSequence = (DuSequence) obj;
-        if (duSequence.duPairs.size() != this.duPairs.size()) {
-            return false;
-        }
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof DuSequence)) {
+			return false;
+		}
 
-        ListIterator<DuPair> duPairsIterator = this.duPairs.listIterator();
+		DuSequence duSequence = (DuSequence) obj;
+		if (duSequence.duPairs.size() != this.duPairs.size()) {
+			return false;
+		}
 
-        while (duPairsIterator.hasNext()) {
-            DuPair duPair1 = duSequence.duPairs.get(duPairsIterator.nextIndex());
-            DuPair duPair2 = duPairsIterator.next();
+		ListIterator<DuPair> duPairsIterator = this.duPairs.listIterator();
 
-            if (!(duPair1.equals(duPair2))) {
-                return false;
-            }
-        }
+		while (duPairsIterator.hasNext()) {
+			DuPair duPair1 = duSequence.duPairs.get(duPairsIterator.nextIndex());
+			DuPair duPair2 = duPairsIterator.next();
 
-        return true;
-    }
+			if (!(duPair1.equals(duPair2))) {
+				return false;
+			}
+		}
 
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		String values = "\nValues:";
+		String instructions = "Def at pos ";
+
+		Iterator<DuPair> duPairsIterator = duPairs.iterator();
+
+		DuPair duPair = duPairsIterator.next();
+		AllocatableValue lastValue = duPair.getValue();
+		values = values + " " + lastValue;
+		instructions = instructions + duPair.getOperandDefPosition() + " in: " + duPair.getDefInstruction();
+
+		while (duPairsIterator.hasNext()) {
+			duPair = duPairsIterator.next();
+
+			lastValue = duPair.getValue();
+			values = values + " -> " + lastValue;
+
+			instructions = instructions + "\nCopy: " + duPair.getDefInstruction();
+		}
+
+		instructions = instructions + "\nUse at pos " + duPair.getOperandUsePosition() + " in: "
+				+ duPair.getUseInstruction();
+
+		return values + "\n" + instructions;
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
 }
