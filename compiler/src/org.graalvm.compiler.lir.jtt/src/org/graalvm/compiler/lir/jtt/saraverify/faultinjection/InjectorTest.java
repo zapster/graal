08fbbe8051e7b0bce63d2fa0c25283c28e8031ac
Schoergi <gabriel.schoerghuber@gmx.at>
2018-04-26 18:40:27 +0200

    changes to InjectorTest
    - injector is now inserted after a RegisterAllocationPhase
    - inserts RegisterAllocationVerificationPhase and VerificationPhase
    only, if SARAVerify is disabled

--- compiler/src/org.graalvm.compiler.lir.jtt/src/org/graalvm/compiler/lir/jtt/saraverify/faultinjection/InjectorTest.java
+++ compiler/src/org.graalvm.compiler.lir.jtt/src/org/graalvm/compiler/lir/jtt/saraverify/faultinjection/InjectorTest.java
@@ -3,7 +3,7 @@ package org.graalvm.compiler.lir.jtt.saraverify.faultinjection;
 import java.util.ListIterator;
 
 import org.graalvm.compiler.jtt.JTTTest;
-import org.graalvm.compiler.lir.alloc.lsra.LinearScanPhase;
+import org.graalvm.compiler.lir.alloc.RegisterAllocationPhase;
 import org.graalvm.compiler.lir.dfa.MarkBasePointersPhase;
 import org.graalvm.compiler.lir.phases.AllocationPhase.AllocationContext;
 import org.graalvm.compiler.lir.phases.LIRPhase;
@@ -17,18 +17,22 @@ public abstract class InjectorTest extends JTTTest {
     @Override
     protected LIRSuites createLIRSuites(OptionValues opts) {
         LIRSuites lirSuites = super.createLIRSuites(opts);
-        RegisterAllocationVerificationPhase registerAllocationVerification = new RegisterAllocationVerificationPhase();
+
         VerificationPhase verification = new VerificationPhase();
+        OptionValues options = this.getDebugContext().getOptions();
 
-        ListIterator<LIRPhase<AllocationContext>> phase = lirSuites.getAllocationStage().findPhase(MarkBasePointersPhase.class);
-        assert phase != null;
-        phase.add(registerAllocationVerification);
+        if (!RegisterAllocationVerificationPhase.Options.SARAVerify.getValue(options)) {
+            RegisterAllocationVerificationPhase registerAllocationVerification = new RegisterAllocationVerificationPhase();
+            ListIterator<LIRPhase<AllocationContext>> phase = lirSuites.getAllocationStage().findPhase(MarkBasePointersPhase.class);
+            assert phase != null;
+            phase.add(registerAllocationVerification);
 
-        phase = lirSuites.getAllocationStage().findPhase(LinearScanPhase.class);
-        assert phase != null;
-        phase.add(verification);
+            phase = lirSuites.getAllocationStage().findPhase(RegisterAllocationPhase.class);
+            assert phase != null;
+            phase.add(verification);
+        }
 
-        phase = lirSuites.getAllocationStage().findPhase(LinearScanPhase.class);
+        ListIterator<LIRPhase<AllocationContext>> phase = lirSuites.getAllocationStage().findPhase(RegisterAllocationPhase.class);
         assert phase != null;
         phase.add(getInjectorPhase());
 
