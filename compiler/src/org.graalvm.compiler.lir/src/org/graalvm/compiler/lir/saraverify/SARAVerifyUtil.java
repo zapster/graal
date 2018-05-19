3dafb20c00875943af2d5b68b3ffbabadefd5cf0
Schoergi <gabriel.schoerghuber@gmx.at>
2018-05-19 20:46:25 +0200

    added method for returning a value with value kind illegal

--- compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/saraverify/SARAVerifyUtil.java
+++ compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/saraverify/SARAVerifyUtil.java
@@ -2,14 +2,24 @@ package org.graalvm.compiler.lir.saraverify;
 
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.graalvm.compiler.debug.GraalError;
+import org.graalvm.compiler.lir.ConstantValue;
 import org.graalvm.compiler.lir.InstructionValueConsumer;
 import org.graalvm.compiler.lir.LIRInstruction;
+import org.graalvm.compiler.lir.LIRValueUtil;
+import org.graalvm.compiler.lir.Variable;
+import org.graalvm.compiler.lir.VirtualStackSlot;
 
+import jdk.vm.ci.code.Register;
+import jdk.vm.ci.code.StackSlot;
+import jdk.vm.ci.code.ValueUtil;
+import jdk.vm.ci.meta.Constant;
 import jdk.vm.ci.meta.Value;
+import jdk.vm.ci.meta.ValueKind;
 
 public class SARAVerifyUtil {
 
@@ -55,5 +65,43 @@ public class SARAVerifyUtil {
             }
         }
         return mergedMap;
+    }
+
+    /**
+     * Returns the argument with illegal value kind.
+     *
+     * @return the argument with illegal value kind
+     */
+    public static Value getValueIllegalValueKind(Value value) {
+        if (value.getValueKind().equals(ValueKind.Illegal)) {
+            return value;
+        }
+
+        if (ValueUtil.isRegister(value)) {
+            Register register = ValueUtil.asRegister(value);
+            return register.asValue(ValueKind.Illegal);
+        }
+
+        if (LIRValueUtil.isVariable(value)) {
+            Variable variable = LIRValueUtil.asVariable(value);
+            return new Variable(ValueKind.Illegal, variable.index);
+        }
+
+        if (ValueUtil.isStackSlot(value)) {
+            StackSlot stackSlot = ValueUtil.asStackSlot(value);
+            return StackSlot.get(ValueKind.Illegal, stackSlot.getRawOffset(), stackSlot.getRawAddFrameSize());
+        }
+
+        if (LIRValueUtil.isVirtualStackSlot(value)) {
+            VirtualStackSlot virtualStackSlot = LIRValueUtil.asVirtualStackSlot(value);
+            return new VirtualStackSlot(virtualStackSlot.getId(), ValueKind.Illegal) {
+            };
+        }
+
+        throw GraalError.shouldNotReachHere("Type " + value.getClass() + "not implemented.");
+    }
+
+    public static ConstantValue asConstantValue(Constant constant) {
+        return new ConstantValue(ValueKind.Illegal, constant);
     }
 }
