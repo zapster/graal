ae2f4de4318c76ea0cc4641d7194094e64250c4b
Schoergi <gabriel.schoerghuber@gmx.at>
2018-07-14 17:02:03 +0200

    changes to injector verification phase
    - added check, if errors were injected into the code
    
    - if errors are injected, then SARA Verify has to report errors
    
    - if no errors are injected, then SARA Verify must not report any errors

--- compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/saraverify/InjectorVerificationPhase.java
+++ compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/saraverify/InjectorVerificationPhase.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 import org.graalvm.compiler.core.common.cfg.AbstractBlockBase;
 import org.graalvm.compiler.core.common.cfg.AbstractControlFlowGraph;
 import org.graalvm.compiler.core.common.cfg.BlockMap;
+import org.graalvm.compiler.debug.DebugContext;
+import org.graalvm.compiler.debug.DebugContext.Scope;
+import org.graalvm.compiler.debug.GraalError;
+import org.graalvm.compiler.debug.Indent;
 import org.graalvm.compiler.lir.LIR;
 import org.graalvm.compiler.lir.LIRInstruction;
 import org.graalvm.compiler.lir.LIRValueUtil;
+import org.graalvm.compiler.lir.StandardOp.LabelOp;
 import org.graalvm.compiler.lir.StandardOp.ValueMoveOp;
 import org.graalvm.compiler.lir.gen.LIRGenerationResult;
 import org.graalvm.compiler.lir.phases.AllocationPhase.AllocationContext;
@@ -18,8 +23,11 @@ import org.graalvm.compiler.options.OptionType;
 
 import jdk.vm.ci.code.TargetDescription;
 import jdk.vm.ci.meta.AllocatableValue;
+import jdk.vm.ci.meta.Value;
 
 public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
+
+    private final static String DEBUG_SCOPE = "SARAVerifyInjectorVerification";
 
     public static class Options {
         // @formatter:off
@@ -30,18 +38,31 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
 
     @Override
     protected void run(TargetDescription target, LIRGenerationResult lirGenRes, AllocationContext context) {
-        injectErrors(lirGenRes);
+        LIR lir = lirGenRes.getLIR();
+        DebugContext debugContext = lir.getDebug();
 
-// try {
-        VerificationPhase.runVerification(lirGenRes, context);
-// assert false : "Injected errors were not detected.";
-// } catch (SARAVerifyError error) {
-//
-// }
+        boolean injectedErrors = injectErrors(lir);
+
+        // log that no errors were injected
+        if (!injectedErrors) {
+            try (Indent i = debugContext.indent(); Scope s = debugContext.scope(DEBUG_SCOPE)) {
+                debugContext.log(3, "%s", "No injected errors.");
+            }
+        }
+
+        try {
+            VerificationPhase.runVerification(lirGenRes, context);
+            if (injectedErrors) {
+                GraalError.shouldNotReachHere("Injected errors were not detected.");
+            }
+        } catch (SARAVerifyError error) {
+            if (!injectedErrors) {
+                GraalError.shouldNotReachHere("SARAVerify error without having injected errors.");
+            }
+        }
     }
 
-    private static void injectErrors(LIRGenerationResult lirGenRes) {
-        LIR lir = lirGenRes.getLIR();
+    private static boolean injectErrors(LIR lir) {
         AbstractControlFlowGraph<?> controlFlowGraph = lir.getControlFlowGraph();
         AbstractBlockBase<?>[] blocks = controlFlowGraph.getBlocks();
 
@@ -49,6 +70,13 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
         int missingLoad = 0;
 
         BlockMap<ArrayList<LIRInstruction>> missingSpillsLoadsMap = new BlockMap<>(controlFlowGraph);
+
+        // get the label instruction from block 0
+        LabelOp functionLabel = (LabelOp) lir.getLIRforBlock(blocks[0]).get(0);
+
+        // get the last incoming value from the label instruction, which is the value for the base
+        // pointer
+        Value basePointer = functionLabel.getIncomingValue(functionLabel.getIncomingSize() - 1);
 
         for (AbstractBlockBase<?> block : blocks) {
             ArrayList<LIRInstruction> instructions = lir.getLIRforBlock(block);
@@ -61,11 +89,11 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
                     AllocatableValue result = valueMoveOp.getResult();
                     AllocatableValue input = valueMoveOp.getInput();
 
-                    if (LIRValueUtil.isStackSlotValue(result) && missingSpill < 5) {
+                    if (LIRValueUtil.isStackSlotValue(result) && !input.equals(basePointer) && missingSpill < 5) {
                         // inject missing spill
                         missingSpillsLoads.add(instruction);
                         missingSpill++;
-                    } else if (LIRValueUtil.isStackSlotValue(input) && missingLoad < 5) {
+                    } else if (LIRValueUtil.isStackSlotValue(input) && !result.equals(basePointer) && missingLoad < 5) {
                         // inject missing load
                         missingSpillsLoads.add(instruction);
                         missingLoad++;
@@ -85,5 +113,7 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
                 instructions.removeAll(missingSpillLoads);
             }
         }
+
+        return missingSpill != 0 || missingLoad != 0;
     }
 }
