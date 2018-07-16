da3a9fe5825e070ccc1da75ef8588172d5a3c2f2
Schoergi <gabriel.schoerghuber@gmx.at>
2018-07-16 16:13:14 +0200

    changes to injector verification phase
    - throws assertion error at the end of the phase to avoid execution of
    the error infected code
    
    - added counter to get the number of injected errors that were detected
    by SARAVerify
    
    - added comment to instructions that have an injected error

--- compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/saraverify/InjectorVerificationPhase.java
+++ compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/saraverify/InjectorVerificationPhase.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.graalvm.compiler.core.common.cfg.AbstractBlockBase;
 import org.graalvm.compiler.core.common.cfg.AbstractControlFlowGraph;
 import org.graalvm.compiler.core.common.cfg.BlockMap;
+import org.graalvm.compiler.debug.CounterKey;
 import org.graalvm.compiler.debug.DebugContext;
 import org.graalvm.compiler.debug.DebugContext.Scope;
 import org.graalvm.compiler.debug.GraalError;
@@ -44,6 +45,9 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
     private static int wrongRegisterAssignmentCount;
     private static int wrongRegisterUseCount;
 
+    public static final CounterKey detectedInjectedErrors = DebugContext.counter("SARAVerifyInjector[injected errors detected]");
+    public static final CounterKey executedTests = DebugContext.counter("SARAVerifyInjector[total number of executed tests]");
+
     public static class Options {
         // @formatter:off
         @Option(help = "Enable fault injector for the static analysis register allocation verification.", type = OptionType.Debug)
@@ -57,8 +61,8 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
         DebugContext debugContext = lir.getDebug();
 
         // boolean injectedErrors = injectMissingSpillLoadErrors(lir);
-        // boolean injectedErrors = injectWrongRegisterAssignmentErrors(lir, context);
-        boolean injectedErrors = injectWrongRegisterUseErrors(lir, context);
+        // boolean injectedErrors = injectWrongRegisterAssignmentErrors(lirGenRes, context);
+        boolean injectedErrors = injectWrongRegisterUseErrors(lirGenRes, context);
 
         // log that no errors were injected
         if (!injectedErrors) {
@@ -73,10 +77,17 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
                 GraalError.shouldNotReachHere("Injected errors were not detected.");
             }
         } catch (SARAVerifyError error) {
+            // sara verify error occured
             if (!injectedErrors) {
                 GraalError.shouldNotReachHere("SARAVerify error without having injected errors.");
+            } else {
+                detectedInjectedErrors.increment(debugContext);
             }
         }
+        executedTests.increment(debugContext);
+
+        // throw error to avoid execution of injected code
+        throw new AssertionError("injector verification phase finished");
     }
 
     private static boolean injectMissingSpillLoadErrors(LIR lir) {
@@ -134,7 +145,8 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
         return missingSpillCount != 0 || missingLoadCount != 0;
     }
 
-    private static boolean injectWrongRegisterAssignmentErrors(LIR lir, AllocationContext context) {
+    private static boolean injectWrongRegisterAssignmentErrors(LIRGenerationResult lirGenRes, AllocationContext context) {
+        LIR lir = lirGenRes.getLIR();
         AbstractBlockBase<?>[] blocks = lir.getControlFlowGraph().getBlocks();
         List<Register> allocatableRegisters = context.registerAllocationConfig.getAllocatableRegisters().asList();
         Random random = new Random();
@@ -161,6 +173,7 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
                                 if (filteredRegisters.size() == 0) {
                                     return value;
                                 }
+                                instruction.setComment(lirGenRes, "injected wrong register assignment error");
 
                                 wrongRegisterAssignmentCount++;
                                 int index = random.nextInt(filteredRegisters.size());
@@ -177,7 +190,8 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
         return wrongRegisterAssignmentCount != 0;
     }
 
-    private static boolean injectWrongRegisterUseErrors(LIR lir, AllocationContext context) {
+    private static boolean injectWrongRegisterUseErrors(LIRGenerationResult lirGenRes, AllocationContext context) {
+        LIR lir = lirGenRes.getLIR();
         AbstractBlockBase<?>[] blocks = lir.getControlFlowGraph().getBlocks();
         List<Register> allocatableRegisters = context.registerAllocationConfig.getAllocatableRegisters().asList();
         Random random = new Random();
@@ -213,6 +227,7 @@ public class InjectorVerificationPhase extends LIRPhase<AllocationContext> {
                                 if (filteredRegisters.size() == 0) {
                                     return value;
                                 }
+                                instruction.setComment(lirGenRes, "injected wrong register use error");
 
                                 wrongRegisterUseCount++;
                                 int index = random.nextInt(filteredRegisters.size());
